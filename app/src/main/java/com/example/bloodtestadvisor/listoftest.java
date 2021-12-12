a482bc7e1c7d7de28907cc2dccee745f36851923
indranil5501 <indranildas5501@gmail.com>
2021-12-12 21:37:07 +0530

    v6

--- app/src/main/java/com/example/bloodtestadvisor/listoftest.java
+++ app/src/main/java/com/example/bloodtestadvisor/listoftest.java
@@ -2,13 +2,53 @@ package com.example.bloodtestadvisor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class listoftest extends AppCompatActivity {
+    Button b1,b2,b3,b4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listoftest);
+        b1= findViewById(R.id.sub1);
+        b2= findViewById(R.id.sub2);
+        b3= findViewById(R.id.sub3);
+        b4= findViewById(R.id.sub4);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(listoftest.this,Rbccinfo.class);
+                startActivity(intent);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(listoftest.this,Wbcc.class);
+                startActivity(intent);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(listoftest.this,Pltsc.class);
+                startActivity(intent);
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(listoftest.this,Hemoc.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bloodtestadvisor/tNc.java
+++ app/src/main/java/com/example/bloodtestadvisor/tNc.java
@@ -2,13 +2,45 @@ package com.example.bloodtestadvisor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class tNc extends AppCompatActivity {
 
+    Button bot,noo;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tnc);
+        noo = findViewById(R.id.no);
+        bot = findViewById(R.id.subtnc);
+        
+        bot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                    Intent intent;
+                    intent = new Intent(tNc.this,listoftest.class);
+                    startActivity(intent);
+            }
+        });
+
+        noo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(tNc.this, "To use the app you have to accept all the terms and condition", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
